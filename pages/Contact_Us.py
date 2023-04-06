d107f0ab995e278d3e9e16824c1305bd08c5551c
schen24 <schen638@yahoo.com>
2023-04-06 23:54:24 +0800

    added send email capability

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,14 +1,21 @@
 import streamlit as sl
 from sendemail import send_email
+import pandas as pd
 
+topics = pd.read_csv('topics.csv')
 sl.header('send an email')
 
-with sl.form(key='msg'):
+#note: break line between subject and from important!!
+# as is space after 'from' prompt!
+with sl.form(key='msg', clear_on_submit=True):
     user_email = sl.text_input(label="your email address:")
+    option = sl.selectbox('pls select topic:', topics['topic'])
     message_body = sl.text_area(label='your message:')
     message = f"""\
 subject:message from website
-from: {user_email}
+
+From: {user_email}
+Topic: {option}
 {message_body}"""
     button = sl.form_submit_button(label='Submit')
     if button:
--- sendemail.py
+++ sendemail.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
      host = 'smtp.gmail.com'
      port = 465
      username = 'shophop247@gmail.com'
-     password = 'idoksrxjgcpifzbz'
+     password = os.getenv('PASSWORD')  #'idoksrxjgcpifzbz'
      receiver = 'shophop247@gmail.com'
      context = ssl.create_default_context()
 
