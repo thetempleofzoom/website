2eae425f60fcf3599f95424f6f47d5e2c5eb8ea7
sy <thetempleofzoom@gmail.com>
2023-05-20 23:34:22 +0800

    environ changed

--- sendemail.py
+++ sendemail.py
@@ -5,9 +5,9 @@ import os
 def send_email(message):
      host = 'smtp.gmail.com'
      port = 465
-     username = 'shophop247@gmail.com'
-     password = os.getenv('PASSWORD')
-     receiver = 'shophop247@gmail.com'
+     username = 'shadowysupercoderssc@gmail.com'
+     password = os.environ.get('SSCPW')
+     receiver = 'shadowysupercoderssc@gmail.com'
      context = ssl.create_default_context()
 
      with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
